aeb15db788edc4ce726278aeddd4713c428bb6c1
Nadine Crisostomo <bcrisostomo@tribalscale.com>
2019-04-04 11:30:56 -0400

    feat: add Footer component

---
+++ src/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const FooterText = styled.div`
+  width: 50vw;
+  margin: auto;
+  text-align: center;
+  font-family: Sans-Serif;
+  font-size: 0.5em;
+`
+
+const Footer = () => <FooterText>© Copyright CoffeeFight Inc.</FooterText>
+
+export default Footer
